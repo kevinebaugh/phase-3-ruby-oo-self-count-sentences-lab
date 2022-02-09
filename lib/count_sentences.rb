f015c90c49ddb4a549ea4203d8bfdb83298abdd2
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-02-09 12:49:10 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    count += self.split(". ").size - 1
+    count += self.split("! ").size - 1
+    count += self.split("? ").size - 1
+    if count > 0
+      count + 1
+    else
+      0
+    end
   end
 end
